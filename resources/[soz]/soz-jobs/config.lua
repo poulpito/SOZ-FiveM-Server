50179a90363c6f9679be41180f02baab006c9a40
Joel Wurtz <joel.wurtz@gmail.com>
2023-11-26 18:06:10 +0100

    feat(dmc): move dmc resell to core

--- resources/[soz]/soz-jobs/config.lua
+++ resources/[soz]/soz-jobs/config.lua
@@ -5,14 +5,4 @@ FoodResell = {
     TargetAccount = "safe_food",
 }
 
-DMCResell = {
-    primary = {
-        coords = vector4(-132.70, -2383.92, 6.00, 174.18), -- Must be vec4
-        ZoneName = "Resell:LSPort:Dmc",
-        SourceAccount = "farm_dmc",
-        TargetAccount = "safe_dmc",
-    },
-    secondary = {ZoneName = "Resell:LSCustom", SourceAccount = "farm_dmc", TargetAccount = "safe_dmc"},
-}
-
 FieldHealthStates = {[0] = "0000", [1] = "1000", [2] = "1100", [3] = "1110", [4] = "1111"}
