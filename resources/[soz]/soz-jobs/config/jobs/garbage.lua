c6aa3d6b225a17ddaaae1ee1af62e096ce18689e
Laikker <120526954+Laikker@users.noreply.github.com>
2023-12-19 02:15:02 +0100

    BB revenue balancing

--- resources/[soz]/soz-jobs/config/jobs/garbage.lua
+++ resources/[soz]/soz-jobs/config/jobs/garbage.lua
@@ -1,12 +1,12 @@
 GarbageConfig = {}
 
 GarbageConfig.SellPrice = {
-    ["default"] = 60,
-    ["sawdust"] = 6,
-    ["petroleum_residue"] = 16,
-    ["seeweed_acid"] = 70,
-    ["torn_garbagebag"] = 32,
-    ["halloween_infernus_garbage"] = 32,
+    ["default"] = 45,
+    ["sawdust"] = 5,
+    ["petroleum_residue"] = 12,
+    ["seeweed_acid"] = 52,
+    ["torn_garbagebag"] = 24,
+    ["halloween_infernus_garbage"] = 24,
 }
 
 GarbageConfig.Processing = {Duration = 60 * 1000, ProcessingStorage = "garbage_processing", ProcessingAmount = 300}
