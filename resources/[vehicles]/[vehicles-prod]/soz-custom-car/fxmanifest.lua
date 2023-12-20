fea3f220ac3507326425ecf2a3ba7ff0a152a03c
Rigonkmalk <rigonkmalk@gmail.com>
2023-12-20 13:59:52 +0100

    New police car
    feat(fdo): add bcso12
    
    fix(fdo): fixed texture for bcso11
    
    New police car
    
    chore(fdo): organize folder
    
    feat(fdo): modify handling of police from sheriff custom's handling
    
    feat(fdo): add lspd12
    
    feat(car): add lspd11
    
    fix(bcso): working on texture livery
    
    bcso11: working on car
    
    bcso11: working on car
    
    feat(bcso): add bcso11 (WIP)
    
    refactor: police and sheriff car rename

--- resources/[vehicles]/[vehicles-prod]/soz-custom-car/fxmanifest.lua
+++ resources/[vehicles]/[vehicles-prod]/soz-custom-car/fxmanifest.lua
@@ -2,7 +2,9 @@ fx_version 'cerulean'
 game 'gta5'
 
 files {
-    "audio/*.rel"
+    "audio/*.rel",
+    "meta/*.meta"
 }
 
 data_file "AUDIO_GAMEDATA" "audio/sozcustomsounds_game.dat"
+data_file "CARCOLS_FILE" "meta/carcols.meta"
