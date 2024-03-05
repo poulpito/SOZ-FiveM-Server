ab68f0427a0aa7fe6590748c29882642dd2c3c59
Joel Wurtz <joel.wurtz@gmail.com>
2024-03-05 14:10:33 +0100

    feat(fine): use repository fines for police instead of configured list

--- resources/[soz]/soz-core/src/nui/components/Police/Fines.tsx
+++ resources/[soz]/soz-core/src/nui/components/Police/Fines.tsx
@@ -1,9 +1,11 @@
 import { fetchNui } from '@public/nui/fetch';
 import { NuiEvent } from '@public/shared/event';
-import { Fines, PoliceJobFineMenuData } from '@public/shared/job/police';
+import { PoliceJobFineMenuData } from '@public/shared/job/police';
 import { MenuType } from '@public/shared/nui/menu';
 import { FunctionComponent } from 'react';
 
+import { RepositoryType } from '../../../shared/repository';
+import { useRepository } from '../../hook/repository';
 import {
     MainMenu,
     Menu,
@@ -34,39 +36,51 @@ export const FinesMenu: FunctionComponent<FinesStateProps> = ({ data }) => {
                     >
                         Amende personnalisée
                     </MenuItemButton>
-                    {Object.keys(Fines).map(category => {
-                        return <MenuItemSubMenuLink id={category}>Catégorie {category}</MenuItemSubMenuLink>;
-                    })}
+                    <MenuItemSubMenuLink id="fine_1">Catégorie 1</MenuItemSubMenuLink>
+                    <MenuItemSubMenuLink id="fine_2">Catégorie 2</MenuItemSubMenuLink>
+                    <MenuItemSubMenuLink id="fine_3">Catégorie 3</MenuItemSubMenuLink>
+                    <MenuItemSubMenuLink id="fine_4">Catégorie 4</MenuItemSubMenuLink>
                 </MenuContent>
             </MainMenu>
-            {Object.keys(Fines).map(category => {
-                return (
-                    <SubMenu id={category}>
-                        <MenuTitle banner={banner}>Catégorie {category}</MenuTitle>
-                        <MenuContent>
-                            {Fines[category].items.map(fine => {
-                                return (
-                                    <MenuItemButton
-                                        onConfirm={async () => {
-                                            await fetchNui(NuiEvent.PolicePreFine, {
-                                                playerServerId: data.playerServerId,
-                                                fine: fine,
-                                            });
-                                        }}
-                                    >
-                                        <div className="flex justify-between">
-                                            <div>{fine.label}</div>
-                                            <div>
-                                                ${fine.price.min} - ${fine.price.max}
-                                            </div>
-                                        </div>
-                                    </MenuItemButton>
-                                );
-                            })}
-                        </MenuContent>
-                    </SubMenu>
-                );
-            })}
+            <FineSubMenu category={1} playerServerId={data.playerServerId} />
+            <FineSubMenu category={2} playerServerId={data.playerServerId} />
+            <FineSubMenu category={3} playerServerId={data.playerServerId} />
+            <FineSubMenu category={4} playerServerId={data.playerServerId} />
         </Menu>
+    );
+};
+
+type FineSubMenuProps = {
+    category: number;
+    playerServerId: number;
+};
+
+const FineSubMenu: FunctionComponent<FineSubMenuProps> = ({ category, playerServerId }) => {
+    const fines = useRepository(RepositoryType.Fine);
+    const finesForCategory = Object.values(fines).filter(fine => fine.category === category);
+
+    return (
+        <SubMenu id={`fine_${category}`}>
+            <MenuTitle banner="https://nui-img/soz/menu_job_gouv">Amendes catégorie {category}</MenuTitle>
+            <MenuContent>
+                {finesForCategory.map(fine => (
+                    <MenuItemButton
+                        onConfirm={async () => {
+                            await fetchNui(NuiEvent.PolicePreFine, {
+                                playerServerId,
+                                fine: fine,
+                            });
+                        }}
+                    >
+                        <div className="flex justify-between">
+                            <div>{fine.label}</div>
+                            <div>
+                                ${fine.price.min} - ${fine.price.max}
+                            </div>
+                        </div>
+                    </MenuItemButton>
+                ))}
+            </MenuContent>
+        </SubMenu>
     );
 };
