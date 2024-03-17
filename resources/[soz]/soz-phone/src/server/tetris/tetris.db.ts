35ccdc2815793ed98117d37c4fed211710b7fd44
Laikker <jeremy.kennel@wanadoo.fr>
2024-03-17 15:14:48 +0100

    Cleanup Snake

--- resources/[soz]/soz-phone/src/server/tetris/tetris.db.ts
+++ resources/[soz]/soz-phone/src/server/tetris/tetris.db.ts
@@ -9,7 +9,7 @@ export class _TetrisDB {
         ]);
     }
 
-    getDBLeaderboard(): Promise<TetrisLeaderboard[]> {
+    getDBLeaderboard(): Promise<LeaderboardInterface[]> {
         return exports.oxmysql.query_async(
             `
             SELECT player.citizenid, phone_profile.avatar, concat(JSON_VALUE(player.charinfo, '$.firstname'), ' ', JSON_VALUE(player.charinfo, '$.lastname')) as player_name, MAX(tetris_score.score) AS score, try_count.game_played
